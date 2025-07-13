ea11e5a708100c918b54115f667e44a709d90786
mylesqpurdue <195245736+mylesqpurdue@users.noreply.github.com>
2025-07-13 16:58:47 -0400

    deploy fixes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/chibay-landing/',
+  base: '/chibay_website/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
